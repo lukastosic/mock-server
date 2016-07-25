59d7a2c798c031981c045db2905cf5dcd9a2cca9
Luka Stosic <lstosic@infodation.nl>
2016-07-25 12:57:49 +0200

    removed not needed test

--- src/test/java/ls/mocks/MockServiceTest.java
+++ src/test/java/ls/mocks/MockServiceTest.java
@@ -1,23 +1,17 @@
 package ls.mocks;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import ls.mocks.MockService;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.ContentEncodingHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
@@ -52,28 +46,14 @@ public class MockServiceTest
     	assertTrue("Should be one option in test file",options.size() == 1);
     }
     
-    public void testExecutingMcok() throws ClientProtocolException, IOException {
-    	
+    public void testExecutingMcok() throws ClientProtocolException, IOException {    	
     	prepareMockServer();
-    	mockService.SetMapping("TEST_MAPPINGS");
-    	
+    	mockService.SetMapping("TEST_MAPPINGS");    	
     	HttpClient client = new DefaultHttpClient();
     	HttpPost post = new HttpPost("http://localhost:"+mockService.GetHttpPort()+"/verify");
     	HttpResponse response = client.execute(post);
     	mockService.StopMockServer();
     	assertTrue("Received status 200",response.getStatusLine().getStatusCode()==200);
-    }
-    
-    public void testMainFile() throws ClientProtocolException, IOException {
-    	mockService.StartMockServer();
-    	mockService.LoadMapppingOptions();
-    	
-    	//prepareMockServer();
-    	mockService.SetMapping("AUTHENTICATION_SERVICE_STANDARD_SETUP");
-    	
-    	HttpClient client = new DefaultHttpClient();
-    	HttpPost post = new HttpPost("http://localhost:"+mockService.GetHttpPort()+"/verify");
-    	HttpResponse response = client.execute(post);
-    	assertTrue("Received status 401",response.getStatusLine().getStatusCode()==401);
-    }
+    }    
+   
 }
